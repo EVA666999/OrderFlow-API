21fdfccea37aaca39ced58b3ca3485e2cb8f9a73
EVA666999 <ukratitelkisok9913@inbox.ru>
2025-02-28 12:54:32 +0300

    del_fake_api

--- api_django/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiDjangoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_django'
--- api_django/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-
-from .views import CategoryViewSet, OrderViewSet, ProductViewSet
-
-api = routers.DefaultRouter()
-api.register('orders', OrderViewSet, basename='orders')
-api.register('products', ProductViewSet, basename='prioducts')
-api.register('category', CategoryViewSet, basename='categoty')
