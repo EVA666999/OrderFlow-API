fcc22daaa31f7060165ce5f03a04afd269ef3fef
EVA666999 <ukratitelkisok9913@inbox.ru>
2025-03-31 16:41:07 +0300

    dada

--- api/api/settings.py
+++ api/api/settings.py
@@ -121,29 +121,29 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB_local", "django"),
-        "USER": os.getenv("POSTGRES_USER_local", "django"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD_local", ""),
-        "HOST": os.getenv("DB_HOST_local", ""),
-        "PORT": os.getenv("DB_PORT_local", 5432),
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         # Меняем настройку Django: теперь для работы будет использоваться
-#         # бэкенд postgresql
 #         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("POSTGRES_DB", "django"),
-#         "USER": os.getenv("POSTGRES_USER", "django"),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
-#         "HOST": os.getenv("DB_HOST", ""),
-#         "PORT": os.getenv("DB_PORT", 5432),
+#         "NAME": os.getenv("POSTGRES_DB_local", "django"),
+#         "USER": os.getenv("POSTGRES_USER_local", "django"),
+#         "PASSWORD": os.getenv("POSTGRES_PASSWORD_local", ""),
+#         "HOST": os.getenv("DB_HOST_local", ""),
+#         "PORT": os.getenv("DB_PORT_local", 5432),
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        # Меняем настройку Django: теперь для работы будет использоваться
+        # бэкенд postgresql
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "django"),
+        "USER": os.getenv("POSTGRES_USER", "django"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", 5432),
+    }
+}
 CELERY_BROKER_URL = f"redis://{os.getenv('REDIS_HOST', 'localhost')}:{os.getenv('REDIS_PORT', 6379)}"
 
 # Password validation
