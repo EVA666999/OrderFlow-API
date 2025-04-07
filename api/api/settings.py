a27e9fb8e7cb218dc4b606d5d0a74dd3bc4d482a
EVA666999 <ukratitelkisok9913@inbox.ru>
2025-04-07 08:46:50 +0300

    dadsa

--- api/api/settings.py
+++ api/api/settings.py
@@ -1,4 +1,10 @@
-import os
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True  
+SECURE_HSTS_PRELOAD = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')import os
 from datetime import timedelta, timezone
 from pathlib import Path
 from celery.schedules import crontab
